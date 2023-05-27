f1066b53c8f1ae918a3638ae461449228a961bcb
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-05-27 11:47:59 -0300

    up

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { User } from 'src/services/User/User.entity';
 import { UserService } from 'src/services/User/UserService';
 
@@ -9,7 +9,7 @@ export class UserController {
   constructor(
     private readonly userService: UserService,
   ) {}
-  @Get()
+  @Post()
   async findAll(@Body() data): Promise<User[]> {
     console.log("🚀 ~ file: user.controller.ts:14 ~ UserController ~ findAll ~ data:", data)
     return this.userService.findUser(data.name,data.password);
