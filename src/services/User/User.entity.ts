2952d9e114db8d5bde47db32332ab5dadefd42b3
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-05-31 11:56:34 -0300

    up user

--- src/services/User/User.entity.ts
+++ src/services/User/User.entity.ts
@@ -14,4 +14,7 @@ export class User {
 
   @Column()
   categoria: string;
+
+  @Column()
+  password: string;
 }
--- src/services/User/UserService.ts
+++ src/services/User/UserService.ts
@@ -7,28 +7,34 @@ import { User } from './User.entity';
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<any>,
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  async findUser(name:string,password:string): Promise<any> {
+  async findUser(name:string,password:string): Promise<User> {
     return this.userRepository.query(
       `select * from "Encanto".user where "name" = '${name}' and "password" = '${password}'`,
     );
   }
   
-/*
-  async update(categoriaCardapio: User): Promise<any> {
+
+  async updatePassowd(categoriaCardapio: User): Promise<any> {
     return this.userRepository.query(
-      'update "Encanto".categoriacardapio set active = $1 , name = $2 where id = $3',
-      [categoriaCardapio.active, categoriaCardapio.name, categoriaCardapio.id],
+      'update "Encanto".user set  password =$1 where id = $2',
+      [categoriaCardapio.password, categoriaCardapio.id],
     );
   }
 
-  async create(categoriaCardapio: User): Promise<any> {
+  async updateAdm(categoriaCardapio: User): Promise<any> {
     return this.userRepository.query(
-      'insert into "Encanto".categoriacardapio (active, name, id) values ($1, $2, $3)',
-      [categoriaCardapio.active, categoriaCardapio.name, categoriaCardapio.id],
+      'update "Encanto".user set active = $1 , categoria = $2 where id = $3',
+      [categoriaCardapio.active, categoriaCardapio.categoria , categoriaCardapio.id],
     );
   }
-*/
+
+async create(categoriaCardapio: User): Promise<User> {
+  return this.userRepository.query(
+    'insert into "Encanto".categoriacardapio ( name ,categoria , id) values ($1, $2, $3)',
+    [categoriaCardapio.name,categoriaCardapio.categoria  , categoriaCardapio.id],
+  );
+}
 }
