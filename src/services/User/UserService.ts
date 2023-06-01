60b9683040114812afd8b70ce74aa1064556531b
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-06-01 16:58:53 -0300

    create user fix

--- src/services/User/UserService.ts
+++ src/services/User/UserService.ts
@@ -39,8 +39,8 @@ export class UserService {
 
 async create(categoriaCardapio: User): Promise<User> {
   return this.userRepository.query(
-    'insert into "Encanto".user ( name ,categoria,password , id) values ($1, $2,,$4 $3)',
-    [categoriaCardapio.name,categoriaCardapio.categoria,categoriaCardapio.password , categoriaCardapio.id],
+    'insert into "Encanto".user ( name ,categoria,password, id,active) values ($1,$2,$3,$4,$5)',
+    [categoriaCardapio.name,categoriaCardapio.categoria,categoriaCardapio.password ,categoriaCardapio.id,categoriaCardapio.active],
   );
 }
 }
