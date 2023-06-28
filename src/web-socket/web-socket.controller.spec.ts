265017bc9d4a26eef52d245e1a710dc9929121e4
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-06-28 10:31:08 -0300

    fix test

--- src/web-socket/web-socket.controller.spec.ts
+++ src/web-socket/web-socket.controller.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { WebSocketController } from './web-socket.controller';
+import { NotificationsController } from './web-socket.controller';
 
-describe('WebSocketController', () => {
-  let controller: WebSocketController;
+describe('NotificationsController', () => {
+  let controller: NotificationsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [WebSocketController],
+      controllers: [NotificationsController],
     }).compile();
 
-    controller = module.get<WebSocketController>(WebSocketController);
+    controller = module.get<NotificationsController>(NotificationsController);
   });
 
   it('should be defined', () => {
