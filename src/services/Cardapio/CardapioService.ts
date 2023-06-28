99fce90fb70a5d509708c6816e32f667a03da4a8
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-06-28 15:48:58 -0300

    up test

--- src/services/Cardapio/CardapioService.ts
+++ src/services/Cardapio/CardapioService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -27,7 +28,8 @@ export class CardapioService {
   }
 
   async update(cardapio: Cardapio): Promise<any> {
-    await this.cacheManager.del('Cardapio');
+    await this.cacheManager.reset();
+
     return this.CardapioRepository.query(
       'update "Encanto".cardapio set active = $1 , name = $2 , price= $3 , description = $4 , category= $5 , sub = $6 , update_at = $7 , update_by= $8 where id = $9',
       [
@@ -45,7 +47,8 @@ export class CardapioService {
   }
 
   async create(cardapio: Cardapio): Promise<any> {
-    await this.cacheManager.del('Cardapio');
+    await this.cacheManager.reset();
+
     return this.CardapioRepository.query(
       'insert into "Encanto".cardapio (active, name, price, description, category, sub, update_at,update_by, id) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
       [
