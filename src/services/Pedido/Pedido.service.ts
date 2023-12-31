af7eb842ad38f62148fa82b4f2488081c218dee7
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-12-30 21:50:32 -0300

    Update INSERT query in PedidoService

--- src/services/Pedido/Pedido.service.ts
+++ src/services/Pedido/Pedido.service.ts
@@ -193,7 +193,7 @@ export class PedidoService {
     return this.pedidoRepository.query(
       `INSERT INTO "Encanto".pedidos_delivery
       (id, qdt, item, valor, idpedido, category, status, created_at)
-      VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)`,
+      VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`,
       [
         data.id,
         data.qdt,
