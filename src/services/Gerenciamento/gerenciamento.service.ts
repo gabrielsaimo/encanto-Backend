7c3bc9ae59a0ba6d01941c0f3fedd7fa3f7ae000
gabrielsaimo <gabrielsaimo68@gmail.com>
2024-01-16 13:14:58 -0300

    remove cache

--- src/services/Gerenciamento/gerenciamento.service.ts
+++ src/services/Gerenciamento/gerenciamento.service.ts
@@ -85,14 +85,10 @@ export class GerenciamentoService {
   }
 
   async getDados(): Promise<any> {
-    const value: any = await this.cacheManager.get('Dados');
-    if (value) {
-      return value;
-    }
     const response = this.GerenciamentoRepository.query(
       'select * from "Encanto".dados_gerais'
     );
-    await this.cacheManager.set('Dados', response, 0);
+
     return response;
   }
 
