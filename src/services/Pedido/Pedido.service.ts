6a46cb8030111adde9a09bd509941ca5decefd65
gabrielsaimo <gabrielsaimo68@gmail.com>
2024-01-24 23:50:55 -0300

    Refactor findStatusPedido query in PedidoService
    This commit refactors the findStatusPedido query in PedidoService to include the pd.item and pd.qdt columns from the pedidos_delivery table. This change provides additional information about the pedido.

--- src/services/Pedido/Pedido.service.ts
+++ src/services/Pedido/Pedido.service.ts
@@ -69,7 +69,7 @@ export class PedidoService {
 
   async findStatusPedido(id: number): Promise<Pedido[]> {
     return this.pedidoRepository.query(
-      `Select p.status,p.valor,p.info, pd.* from "Encanto".pedido p join "Encanto".pedidos_delivery pd on(p.id = pd.idpedido)  where p.id = $1;;`,
+      `Select p.status,p.valor,p.info, pd.item,pd.qdt,pd.idpedido from "Encanto".pedido p join "Encanto".pedidos_delivery pd on(p.id = pd.idpedido)  where p.id = $1;`,
       [id]
     );
   }
